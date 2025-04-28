c1777274d9539d6eb4750fab8a00db84fc362166
Emir Uzar <emiruzar07@icloud.com>
2025-04-28 19:30:46 +0300

    v0.18 Damageable Fires Added

--- Assets/_GameAssets/Scripts/Managers/GameManager.cs
+++ Assets/_GameAssets/Scripts/Managers/GameManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -14,6 +15,8 @@ public class GameManager : MonoBehaviour
 
     [Header("Settings")]
     [SerializeField] private int _maxEggCount = 5;
+    [SerializeField] private float _delay;
+
 
     private GameState _currentGameState;
     private int _currentEggCount;
@@ -21,6 +24,16 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         Instance = this;
+    }
+
+    private void Start() 
+    {
+        HealthManager.Instance.OnPlayerDeath += HealthManager_OnPlayerDeath;
+    }
+
+    private void HealthManager_OnPlayerDeath()
+    {
+        StartCoroutine(OnGameOver()); //playerTransform, isCatCatched
     }
 
     private void OnEnable()
@@ -46,6 +59,13 @@ public class GameManager : MonoBehaviour
             ChangeGameState(GameState.GameOver);
             _winLoseUI.OnGameWin();
         }
+    }
+
+    private IEnumerator OnGameOver()
+    {
+        yield return new WaitForSeconds(_delay);
+        ChangeGameState(GameState.GameOver);
+        _winLoseUI.OnGameLoose();
     }
 
     public GameState GetCurrentGameState()
--- Assets/_GameAssets/Scripts/Managers/HealthManager.cs
+++ Assets/_GameAssets/Scripts/Managers/HealthManager.cs
@@ -1,9 +1,23 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class HealthManager : MonoBehaviour
 {
+    public static HealthManager Instance{get; private set;}
+    public event Action OnPlayerDeath;
+
+    [Header("References")]
+    [SerializeField] private PlayerHealthUI _playerHealthUI;
+
+    [Header("Settings")]
     [SerializeField] private int _maxHealth = 3;
     private int _currentHealth;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     private void Start()
     {
@@ -15,10 +29,11 @@ public class HealthManager : MonoBehaviour
         if(_currentHealth > 0)
         {
             _currentHealth -= damageAmount;
+            _playerHealthUI.AnimateDamage();
             
             if(_currentHealth <= 0)
             {
-
+                OnPlayerDeath?.Invoke();
             }
         }
     }
