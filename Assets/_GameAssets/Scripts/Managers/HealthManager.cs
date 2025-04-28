3a863f4e6cb601a85ecb1dd1ecfca34d14427777
Emir Uzar <emiruzar07@icloud.com>
2025-04-28 05:02:22 +0300

    v0.12 Health System Edit

---
+++ Assets/_GameAssets/Scripts/Managers/HealthManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    [SerializeField] private int _maxHealth = 3;
+    private int _currentHealth;
+
+    private void Start()
+    {
+        _currentHealth = _maxHealth;
+    }
+
+    public void Damage(int damageAmount)
+    {
+        if(_currentHealth > 0)
+        {
+            _currentHealth -= damageAmount;
+            
+            if(_currentHealth <= 0)
+            {
+
+            }
+        }
+    }
+
+    public void Heal (int healAmount)
+    {
+        if(_currentHealth < _maxHealth)
+        {
+            _currentHealth = Mathf.Min(_currentHealth + healAmount, _maxHealth);
+        }
+    }
+}
