68b3656e8ededddd1115a880f2c665e08f1d19b7
Emir Uzar <emiruzar07@icloud.com>
2025-04-28 07:50:56 +0300

    v0.13 Timer System Added

---
+++ Assets/_GameAssets/Scripts/UI/TimerUI.cs
@@ -0,0 +1,45 @@
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+public class TimerUI : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private RectTransform _timerRotatablaTransform;
+    [SerializeField] private TMP_Text _timerText;
+
+    [Header("Settings")]
+    [SerializeField] private float _rotationDuration;
+    [SerializeField] private Ease _rotattionEase;
+
+    private float _elapsedTime; 
+ 
+    private void Start()
+    {
+        PlayRotationAnimation();
+        StartTimer();
+    }
+
+    private void PlayRotationAnimation()
+    {
+        _timerRotatablaTransform.DORotate(new Vector3(0f, 0f, -360), _rotationDuration, RotateMode.FastBeyond360)
+            .SetLoops(-1, LoopType.Restart)
+            .SetEase(_rotattionEase);
+    }
+
+    private void StartTimer()
+    {
+        _elapsedTime = 0f;
+        InvokeRepeating(nameof(UpdateTimerUI), 0f, 1f);
+    }
+
+    private void UpdateTimerUI()
+    {
+        _elapsedTime += 1f;
+
+        int minutes = Mathf.FloorToInt(_elapsedTime / 60f);
+        int seconds = Mathf.FloorToInt(_elapsedTime % 60f);
+
+        _timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
