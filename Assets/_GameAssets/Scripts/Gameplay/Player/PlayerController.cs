35ede7b3c183508aaa0ffa7ee5185da83038d596
Emir Uzar <emiruzar07@icloud.com>
2025-04-28 04:33:32 +0300

    v0.11 UI Elements & Systems Added

--- Assets/_GameAssets/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/_GameAssets/Scripts/Gameplay/Player/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public event Action OnPlayerJumped; 
+    public event Action<PlayerState> OnPlayerStateChanged;
 
     [Header("References")]
     [SerializeField] private Transform _orientationTransform;
@@ -102,6 +103,7 @@ public class PlayerController : MonoBehaviour
          if(newState != currentState)
             {
                 _stateController.ChangeState(newState);
+                OnPlayerStateChanged?.Invoke(newState);
             }
 
         //Debug.Log(newState);
