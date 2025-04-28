ff5730d870ecf2d0b33a5b2ded33bdf585b48e29
Emir Uzar <emiruzar07@icloud.com>
2025-04-28 08:32:46 +0300

    v0.14 Win Condition Eggs & UI Added

---
+++ Assets/_GameAssets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance {get; private set;}
+
+    [Header("Preferences")]
+    [SerializeField] private EggCounterUI _eggCounterUI;
+
+    [Header("Settings")]
+    [SerializeField] private int _maxEggCount = 5;
+
+    private int _currentEggCount;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void OnEggCollected()
+    {
+        _currentEggCount++;
+        _eggCounterUI.SetEggCounterText(_currentEggCount, _maxEggCount);
+
+        if(_currentEggCount == _maxEggCount)
+        {
+            Debug.Log("Game Win!");
+            _eggCounterUI.SetEggCompleted();
+        }
+        Debug.Log("Egg Count: " + _currentEggCount);
+    }
+}
